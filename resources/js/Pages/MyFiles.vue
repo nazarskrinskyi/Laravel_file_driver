1fa7cc75641099a43f0191d1f8e4f29c49166895
Nazar <nazarskrinskyi@gmail.com>
2023-10-29 12:41:41 +0200

    added file upload animation + scroll for files + error message modal

--- resources/js/Pages/MyFiles.vue
+++ resources/js/Pages/MyFiles.vue
@@ -5,8 +5,10 @@
                 <li v-for="ancestor in ancestors.data" :key="ancestor.id" class="inline-flex items-center">
                     <Link v-if="!ancestor.parent_id" :href="route('myFiles')"
                           class="inline-flex items-center text-sm font-medium text-gray-800 dark:text-gray-600 dark:hover:text-black dark:hover:font-semibold">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-1 text-gray-950">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" class="w-6 h-6 mr-1 text-gray-950">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
                         </svg>
                         My Files
                     </Link>
@@ -25,31 +27,36 @@
                 </li>
             </ol>
         </nav>
+        <div class="flex-1 overflow-auto">
+            <table class="min-w-full">
+                <thead class="border border-b-black">
+                <tr>
+                    <th class="font-semibold border border-r-black border-b-black">Name</th>
+                    <th class="font-semibold border border-r-black border-b-black">Owner</th>
+                    <th class="font-semibold border border-r-black border-b-black">Size</th>
+                    <th class="font-semibold border border-r-black border-b-black">Last Modified</th>
+                    <th class="font-semibold border border-r-black border-b-black">Created</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="file in files.data" :class="file.is_folder === 1 ? 'bg-amber-300' : ''"
+                    @dblclick.prevent="openFolder(file)"
+                    class="cursor-pointer transition duration-300 ease-in-out hover:bg-gray-300" :key="file.id">
 
-        <table class="min-w-full">
-            <thead class="border border-b-black">
-            <tr>
-                <th class="font-semibold border border-r-black border-b-black">Name</th>
-                <th class="font-semibold border border-r-black border-b-black">Owner</th>
-                <th class="font-semibold border border-r-black border-b-black">Size</th>
-                <th class="font-semibold border border-r-black border-b-black">Last Modified</th>
-                <th class="font-semibold border border-r-black border-b-black">Created</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="file in files.data" :class="file.is_folder === 1 ? 'bg-amber-300' : ''" @dblclick.prevent="openFolder(file)"
-                class="cursor-pointer transition duration-300 ease-in-out hover:bg-gray-300" :key="file.id">
-
-                <td class="cursor-pointer p-2 font-medium border border-r-black  border-b-black"><span class="flex items-center"><FileIcon :file="file" class="pl-3"/><span class="pl-4">{{ file.name }}</span></span></td>
-                <td class="p-2 font-medium border border-r-black  border-b-black">{{ file.owner }}</td>
-                <td class="p-2 font-medium border border-r-black  border-b-black">{{ file.size }}</td>
-                <td class="p-2 font-medium border border-r-black  border-b-black">{{ file.updated_at }}</td>
-                <td class="p-2 font-medium border border-r-black  border-b-black">{{ file.created_at }}</td>
-            </tr>
-            </tbody>
-        </table>
-        <div v-if="!files.data.length" class="py-8 text-center font-semibold text-4xl text-gray-400">
-            <h1>There no data in laravel driver</h1>
+                    <td class="cursor-pointer p-2 font-medium border border-r-black  border-b-black"><span
+                        class="flex items-center"><FileIcon :file="file" class="pl-3"/><span class="pl-4">{{
+                            file.name
+                        }}</span></span></td>
+                    <td class="p-2 font-medium border border-r-black  border-b-black">{{ file.owner }}</td>
+                    <td class="p-2 font-medium border border-r-black  border-b-black">{{ file.size }}</td>
+                    <td class="p-2 font-medium border border-r-black  border-b-black">{{ file.updated_at }}</td>
+                    <td class="p-2 font-medium border border-r-black  border-b-black">{{ file.created_at }}</td>
+                </tr>
+                </tbody>
+            </table>
+            <div v-if="!files.data.length" class="py-8 text-center font-semibold text-4xl text-gray-400">
+                <h1>There no data in laravel driver</h1>
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
@@ -59,9 +66,7 @@
 import AuthenticatedLayout from "../Layouts/AuthenticatedLayout.vue";
 import {router} from "@inertiajs/vue3";
 import {Link} from "@inertiajs/vue3";
-import {computed} from "vue";
 import FileIcon from "../Components/app/FileIcon.vue";
-import {f} from "../../../public/build/assets/app-ebc2819d.js";
 //refs
 
 //props & emits
@@ -69,6 +74,7 @@ const {files} = defineProps({
     files: Object,
     ancestors: Object
 })
+
 //methods
 function openFolder(file) {
     if (!file.is_folder) {
@@ -79,7 +85,6 @@ function openFolder(file) {
 
 //computed
 // hooks
-
 
 
 </script>
--- resources/js/event-mitt.js
+++ resources/js/event-mitt.js
@@ -1,6 +1,10 @@
 import mitt from "mitt";
 
 export const FILE_UPLOAD_STARTED = 'FILE_UPLOAD_STARTED';
+export const SHOW_ERROR_MESSAGE = 'SHOW_ERROR_MESSAGE';
 export const emitter = mitt();
 
+export function showErrorMessage(message) {
+    emitter.emit(SHOW_ERROR_MESSAGE, {message})
+}
 
