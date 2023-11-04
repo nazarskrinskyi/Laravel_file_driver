a2890f943a578c2ec4229ee99672bf72c9ded476
Nazar <nazarskrinskyi@gmail.com>
2023-11-04 19:43:49 +0200

    Design Confirmation Modal window

--- resources/js/Components/app/ConfirmationModal.vue
+++ resources/js/Components/app/ConfirmationModal.vue
@@ -1,46 +1,43 @@
-
 <template>
     <Modal :show="show" max-width="md">
         <div class="p-6">
-            <h2 class="mb-2 text-xl text-red-500 font-semibold">Error</h2>
-            <p>{{message}}</p>
-            <div class="flex justify-end">
-                <PrimaryButton @click="close">OK</PrimaryButton>
+            <h2 class="text-2xl font-semibold text-center">Please Confirm</h2>
+            <p>{{ message }}</p>
+            <div class="mt-4 flex justify-end">
+                <div class="pr-3">
+                    <PrimaryButton @click="emit('confirm')" @keyup.enter="emit('confirm')">Confirm</PrimaryButton>
+                </div>
+                <div>
+                    <DangerButton @click="emit('cancel')" @keyup.esc="emit('cancel')">Cancel</DangerButton>
+                </div>
             </div>
         </div>
     </Modal>
 </template>
 
-<script setup lang="ts">
+<script setup>
+// Imports
 import Modal from "../Modal.vue";
-import {onMounted, ref} from "vue";
+import DangerButton from "../DangerButton.vue";
 import PrimaryButton from "../PrimaryButton.vue";
-import {emitter, SHOW_ERROR_MESSAGE} from "../../event-mitt.js";
 
-//imports
+// Uses
 
-//refs
+// Refs
 
-//props & emits
-const show = ref(false);
-const message = ref('');
+// Props & Emit
+const props = defineProps({
+    show: Boolean,
+    message: String
+})
+const emit = defineEmits(['cancel', 'confirm'])
 
-const emit = defineEmits(['close'])
-//methods
-function close() {
-    show.value = false;
-    message.value = '';
-}
+// Computed
 
-//computed
+// Methods
 
-// hooks
-onMounted(() => {
-    emitter.on(SHOW_ERROR_MESSAGE, ({message: msg}) => {
-        show.value = true;
-        message.value = msg;
-    })
-});
+// Hooks
+
 </script>
 
 <style scoped>
