b6ff0db90dc6cdb55beffd3afd364f0a90b11024
Nazar <nazarskrinskyi@gmail.com>
2023-11-04 19:53:22 +0200

    Implement download functionality that properly analyze what data comes to download file, files, folder, folders

--- resources/js/event-mitt.js
+++ resources/js/event-mitt.js
@@ -2,9 +2,19 @@ import mitt from "mitt";
 
 export const FILE_UPLOAD_STARTED = 'FILE_UPLOAD_STARTED';
 export const SHOW_ERROR_MESSAGE = 'SHOW_ERROR_MESSAGE';
+export const SHOW_NOTIFICATION = 'SHOW_NOTIFICATION';
 export const emitter = mitt();
 
 export function showErrorMessage(message) {
     emitter.emit(SHOW_ERROR_MESSAGE, {message})
 }
+
+export function showSuccessNotification(message) {
+    emitter.emit(SHOW_NOTIFICATION, {type: 'success', message})
+}
+export function showErrorNotification(message) {
+    emitter.emit(SHOW_NOTIFICATION, {type: 'error', message})
+
+}
+
 
